167106aa57c7c018d4d59e5dc5b4bdefcd57f608
waodiao <851298395@qq.com>
2018-09-14 23:05:43 +0800

    https

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const Koa = require('koa');
-const app = new Koa();
+const http = require('http');
+const https = require('https');
+const enforceHttps = require('koa-sslify');
 const fs = require('fs');
 
+const app = new Koa();
+
+app.use(enforceHttps());
+
+const options = {
+    key: fs.readFileSync('./ssl/server.key'),  //ssl文件路径
+    cert: fs.readFileSync('./ssl/server.pem')  //ssl文件路径
+};
+
 app.use(async ctx => {
-  const data = fs.readFileSync('/Users/tianxiadayu/demo/ksaq/package.json', 'utf-8');
+  const data = fs.readFileSync('/diao/AliServer/test.txt', 'utf-8');
   ctx.body = data;
 });
+
+https.createServer(options, app.callback()).listen(443);
+//app.listen(3000)
+//console.log('服务启动成功，监听3000端口')
