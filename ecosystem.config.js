738d197c6ef62541b65adbe0fe3aaa0fc58f6ecc
waodiao <851298395@qq.com>
2018-09-20 10:25:50 +0800

    add max_memory_restart size

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script    : 'server.js',
     "exec_mode": "cluster_mode",
     "cron_restart": "01 01 * * * *",
+    "max_memory_restart": "300M",
     "log_date_format"  : "YYYY-MM-DD HH:mm Z",
   }],
 };
