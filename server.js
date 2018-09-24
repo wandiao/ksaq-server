bad58ef2089f6b756b4c0ce79ccee2d77220f08b
wandiao <851298395@qq.com>
2018-09-24 15:06:15 +0800

    add redis

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const https = require('https');
 const enforceHttps = require('koa-sslify');
 const fs = require('fs');
 
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+const redis = require('redis');
+
+
 const app = new Koa();
+const client = redis.createClient(6379, "127.0.0.1");
+
+app.use(session({
+  store: redisStore({
+    client,
+    db:1,
+  })
+}));
 
 app.use(enforceHttps());
 
@@ -13,13 +26,12 @@ const options = {
     cert: fs.readFileSync('./ssl/server.pem')  //ssl文件路径
 };
 
-let data = null;
-setInterval(() => {
-  data =  fs.readFileSync('/diao/AliServer/test.txt', 'utf-8');
-}, 1000)
-
 app.use(async ctx => {
-  ctx.body = data;
+  const session = this.session;
+  session.count = session.count || 0;
+  session.count++;
+  this.body = session.count;
+
 });
 
 https.createServer(options, app.callback()).listen(443);
